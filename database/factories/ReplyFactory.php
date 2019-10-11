d61c2de0969a60ccaab849f6b56cd0e1c762a516
mohasin <mohasin2911@gmail.com>
2019-10-11 16:19:37 +0600

    creating demo data using factory

--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -1,9 +1,17 @@
 <?php
 
+use App\User;
+use App\Models\Question;
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Reply::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker->text,
+        'question_id' => function(){
+            return Question::all()->random();
+        },
+        'user_id' => function(){
+            return User::all()->random();
+        }
     ];
 });
